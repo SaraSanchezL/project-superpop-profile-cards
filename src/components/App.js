88bd31046043a4c954f431fb465d17bcb7cf2238
SaraSanchezL <sarasanchezlopez93@gmail.com>
2022-01-11 12:31:27 +0100

    add template React

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import logo from '../images/logo.svg';
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      Hola Adalabers!
+      <img src={logo} alt="Imagen de ejemplo" />
+    </div>
+  );
+}
+
+export default App;
